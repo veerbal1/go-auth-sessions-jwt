a33b5ce3b50a9477856d1ea9f2333e06ecf2ceed
veerbal1 <veerbalsingh1@gmail.com>
2026-06-14 13:11:55 +0530

    Update database migration files: add UUID primary key to user_roles table and ensure users table is dropped in down migration.

--- migrations/000001_create_users_roles_tables.up.sql
+++ migrations/000001_create_users_roles_tables.up.sql
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS roles (
 );
 
 CREATE TABLE IF NOT EXISTS user_roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     role_id UUID NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
     UNIQUE (user_id, role_id),
